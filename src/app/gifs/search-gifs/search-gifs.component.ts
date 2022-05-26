70a5cc05f53a1fd58eb33a7da5e59e26c2800d95
WhitenAND07 <jordi14and@gmail.com>
2022-05-26 11:34:31 +0200

    Work about ViewChild

--- src/app/gifs/search-gifs/search-gifs.component.ts
+++ src/app/gifs/search-gifs/search-gifs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-gifs',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
 })
 export class SearchGIFsComponent {
 
-  constructor() { }
+  constructor() {}
 
+  //https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  search(){
+    const valor: string = this.txtBuscar.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+  }
 }
